48abfd7935ddfbf3c476734c6784f85311200a1e
Milton Raza <milton.raza@gmail.com>
2025-03-09 13:07:13 -0500

    ref: Cambios

--- src/main/java/ec/practica/springboot/model/Movimiento.java
+++
@@ -1,5 +0,0 @@
-package ec.practica.springboot.model;
-
-public class Movimiento {
-
-}
--- src/main/java/ec/practica/springboot/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package ec.practica.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ec.practica.springboot.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-}
